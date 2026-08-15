407c150eade668f44e75f844c1e8dd81f4bd0f21
Nischay Nayak <nischay.ug25it059@bitsindri.ac.in>
2026-08-16 02:26:36 +0530

    fix: serve Firebase modules in production

---
+++ public/firebase/firebase.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    query,
+    orderBy
+} from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBTZe8ONLkvqDcYSDx4zvczus-kxXLLwtc",
+  authDomain: "project-nexus-v3.firebaseapp.com",
+  projectId: "project-nexus-v3",
+  storageBucket: "project-nexus-v3.firebasestorage.app",
+  messagingSenderId: "89012047732",
+  appId: "1:89012047732:web:a8b0aba1c6d9aeab41a621"
+};
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Firestore Database
+const db = getFirestore(app);
+
+// Authentication
+const auth = getAuth(app);
+
+// Google Login Provider
+const provider = new GoogleAuthProvider();
+
+const storage = getStorage(app);
+
+// Export everything
+export {
+    app,
+    db,
+    auth,
+    provider,
+    storage,
+
+    collection,
+    getDocs,
+    query,
+    orderBy
+};
